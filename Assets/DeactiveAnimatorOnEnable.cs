eb6c230749a668b449f13dd213a26d79ae56d525
Gabriel FOURNIER <gabfou95@gmail.com>
2018-09-29 00:33:53 +0200

    dfs

---
+++ Assets/DeactiveAnimatorOnEnable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactiveAnimatorOnEnable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void OnEnable() {
+		GetComponent<Animator>().enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
