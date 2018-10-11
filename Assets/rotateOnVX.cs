3253b871652a4e2f97501fc34905bb88ce3ba341
gabfou <gabfou95@gmail.com>
2018-10-12 01:23:38 +0200

    fcsed

---
+++ Assets/rotateOnVX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateOnVX : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float mult = 5;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 0, -mult * Time.deltaTime * rb.velocity.x));
+	}
+}
