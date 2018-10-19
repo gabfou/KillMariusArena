b357815f0576676ec5e2ad4a438b4c841fb58015
Gabriel FOURNIER <gabfou95@gmail.com>
2018-10-19 23:36:42 +0200

    sdfsfd

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+	public string nexTSceneName;
+	public float WaitInSeconds = 2;
+	public string tagAccepted = "Player";
+	// Use this for initialization
+	
+	IEnumerator WaitForNext()
+	{
+		yield return new WaitForSeconds(WaitInSeconds);
+		SceneManager.LoadScene(nexTSceneName);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == tagAccepted)
+			StartCoroutine(WaitForNext());
+	}
+}
