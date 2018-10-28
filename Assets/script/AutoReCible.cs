2969f7a0ce2fcbc7959a09cafe06404615ca2888
Gabriel FOURNIER <gabfou95@gmail.com>
2018-10-28 22:32:35 +0100

    dffsd

---
+++ Assets/script/AutoReCible.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoReCible : MonoBehaviour {
+
+	Transform Cible;
+	public float nbReCible = 1;
+	public enum ReCibleBehaviour {Boomerang, CiblePlayer};
+	public ReCibleBehaviour reCibleBehaviour = ReCibleBehaviour.Boomerang;
+	bool isInRecible = false;
+	public float timeToStop = 1;
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start ()
+	{
+		Cible = GameManager.instance.player.transform;
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	IEnumerator ReCibleBoomerang()
+	{
+		float time = 0;
+		Vector2 velocity = rb.velocity;
+		while (timeToStop > time)
+		{
+			float t = (timeToStop - time * 2) / timeToStop;
+			time += Time.deltaTime;
+			rb.velocity = velocity * t;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator ReCiblePlayer()
+	{
+		float time = 0;
+		Vector2 velocity = rb.velocity;
+		Vector2 newDir = Vector2.zero;
+		while (timeToStop > time)
+		{
+			float t = (timeToStop - time * 2) / timeToStop;
+			time += Time.deltaTime;
+			if (t > 0)
+				rb.velocity = velocity * t;
+			else
+			{
+				if (newDir == Vector2.zero)
+					newDir = (Cible.position - transform.position).normalized;
+				rb.velocity = velocity.magnitude * newDir * -t;
+			}
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (nbReCible > 0 && isInRecible == false
+			&& Vector2.Distance((Vector2)transform.position + rb.velocity.normalized, Cible.position) > Vector2.Distance(transform.position, Cible.position))
+		{
+			isInRecible = true;
+			if (reCibleBehaviour == ReCibleBehaviour.Boomerang)
+				StartCoroutine(ReCibleBoomerang());
+			else if (reCibleBehaviour == ReCibleBehaviour.CiblePlayer)
+				StartCoroutine(ReCiblePlayer());;
+			nbReCible--;
+		}
+	}
+}
--- Assets/script/enableAnimatorOnTrigger.cs
+++ Assets/script/enableAnimatorOnTrigger.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [RequireComponent(typeof(Animator))]
 public class enableAnimatorOnTrigger : MonoBehaviour {
 
 
 	public List<string> tagAccept;
+	public bool autoDestroyLastTrigger = false;
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (tagAccept.Count == 0 || tagAccept.Contains(other.tag))
+		{
 			GetComponent<Animator>().enabled = true;
+			if (autoDestroyLastTrigger)
+				GameObject.Destroy(GetComponents<Collider2D>().Where(c => c.isTrigger).LastOrDefault());
+		}
 	}
 }
