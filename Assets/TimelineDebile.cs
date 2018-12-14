09b11dd0711b0de65dfb5bc4163a60401038cefa
Gabriel FOURNIER <gabfou95@gmail.com>
2018-12-14 23:47:07 +0100

    daf

---
+++ Assets/TimelineDebile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineDebile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       GetComponent<PlayableDirector>().playableGraph.GetRootPlayable(0).SetSpeed(2.5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
