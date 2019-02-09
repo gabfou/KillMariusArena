a6957a2ef0fcc04bb27c3e08f7bf55f5ab26990f
gabfou <gabfou95@gmail.com>
2019-02-09 15:18:16 +0100

    fds

--- Assets/Boss2Fatality.cs
+++ Assets/Boss2Fatality.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Boss2Fatality : MonoBehaviour
 {
+    PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameManager.instance.player;
+        player.cannotmove = true;
+        player.canControle = false;
     }
 
+    Vector2 r = Vector2.zero;
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 target = transform.position + (player.transform.position - transform.position).normalized * 2;
+        player.transform.position = Vector2.SmoothDamp(player.transform.position, target, ref r, 1);
     }
 }
