39def93056a0a10e72f4628a30137775867e427a
Gabriel FOURNIER <gabfou95@gmail.com>
2019-02-11 12:26:31 +0100

    fdsf

---
+++ Assets/PrepNextLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PrepNextLevel : MonoBehaviour
+{
+    public string nextSceneName;
+    AsyncOperation ao = null;
+
+
+
+    public ActiveScene()
+    {
+        if (ao != null)
+            ao.allowSceneActivation = true;
+    }
+
+    public PreLoadScene()
+    {
+        if (ao == null)
+        {
+        	ao = SceneManager.LoadSceneAsync(nextSceneName);
+    		ao.allowSceneActivation = false;
+        }
+    }
+}
