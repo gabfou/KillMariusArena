8d4708df7279950b32add1d4a8d0aaced14e23c9
Gabriel FOURNIER <gabfou95@gmail.com>
2019-03-07 23:44:16 +0100

    fdsf

--- Assets/script/Utils/NextLevelOnEnable.cs
+++ Assets/script/Utils/NextLevelOnEnable.cs
@@ -13,10 +13,12 @@ public class NextLevelOnEnable : MonoBehaviour
 	{
 		AsyncOperation ao;
 
+		Application.backgroundLoadingPriority = ThreadPriority.Low; // do not stutter
 		yield return null; //wait one frame in case the scene just loaded
 		ao = SceneManager.LoadSceneAsync(nexTSceneName);
 		ao.allowSceneActivation = false;
 		yield return new WaitForSeconds(WaitInSeconds);
+		Application.backgroundLoadingPriority = ThreadPriority.High; // HAAAAAAAAAA LOAD THE SCENE WE ARE LATE
 		ao.allowSceneActivation = true;
 	}
 
