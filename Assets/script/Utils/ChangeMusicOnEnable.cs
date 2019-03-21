76cff7e0a3914df3663f20728ae2e876aefcb9e3
Gabriel FOURNIER <gabfou95@gmail.com>
2019-03-22 00:15:02 +0100

    dsff

--- Assets/script/Utils/ChangeMusicOnEnable.cs
+++ Assets/script/Utils/ChangeMusicOnEnable.cs
@@ -9,9 +9,17 @@ public class ChangeMusicOnEnable : MonoBehaviour
     public AudioClip music;
     [Range(0,1)]public float volume = 1;
     public float timeToFadeInSec = 0.5f;
+    public bool onlyFirtstTime = false;
 
 
     void OnEnable() {
+        if (onlyFirtstTime == true)
+        {
+            float id = transform.position.sqrMagnitude;
+            if (GameManager.instance.save.listOfObjectAlreadyUse.Contains(id))
+		    	return ;
+            GameManager.instance.save.listOfObjectAlreadyUse.Add(id);
+        }
         GameManager.instance.ChangeMusic(intro, music, timeToFadeInSec, volumeOfIntro, volume);
     } 
 }
