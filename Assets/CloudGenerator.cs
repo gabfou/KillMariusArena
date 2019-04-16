a20ea94ca7a0df42c93ec378cea46be22594aa13
Gabriel FOURNIER <gabfou95@gmail.com>
2019-04-16 19:07:30 +0200

    fdsf

---
+++ Assets/CloudGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudGenerator : MonoBehaviour
+{
+    public List<GameObject> clouds;
+    public float timeBetweenNuage = 0.2f;
+    public float timeRandome = 3;
+
+    IEnumerator NuageSpawner()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeBetweenNuage + Random.Range(0f, timeRandome));
+            GameObject.Instantiate(clouds[Random.Range(0, clouds.Count)]);
+        }
+    }
+
+    private void Start() {
+        if (clouds.Count > 0)
+            StartCoroutine(NuageSpawner());
+        else
+            Debug.Log("mo clouds in cloudmanager name: " + name);
+    }
+}
---
+++ Assets/nuage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nuage : MonoBehaviour
+{
+    Transform player;
+    public float speedMin = 5;
+    public float speedMax = 15;
+    float speed;
+    float startDiff;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        player = GameManager.instance.player.transform;
+        speed = Random.Range(speedMin, speedMax);
+        startDiff = Mathf.Abs(transform.position.x - player.position.x) * 1.2f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (startDiff < Mathf.Abs(transform.position.x - player.position.x))
+            GameObject.Destroy(gameObject);
+        else
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+    }
+}
